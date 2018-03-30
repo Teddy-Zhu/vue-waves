4a11d6a14f1f536b2b64a71ca6eb41c140e60ebc
sa-inu <uovo.aureo.000@gmail.com>
2018-03-31 02:01:56 +0900

    update packages

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,13 +1,13 @@
 export default {
     entry: './index.js',
     output: {
-        path: './dist',
+        path: __dirname + '/dist',
         filename: 'vue-waves.js',
         library: 'VueWaves',
         libraryTarget: 'umd',
     },
     module: {
-        loaders: [
+        rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
             { test: /\.css$/, loader: "style-loader!css-loader" },
         ]
